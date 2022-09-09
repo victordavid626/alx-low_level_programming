269cecb3100a525afa521a2a342a809813a6400d
victordavid626 <victordavid626@gmail.com>
2022-09-08 17:49:42 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- *   * Return: 0
- *    */
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 		printf("Size of a char: %i byte(s)\n", sizeof(char));
