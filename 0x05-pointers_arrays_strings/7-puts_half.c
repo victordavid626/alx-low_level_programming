1cf2d07a04fcc24338afad6db869ac130d8c5a2a
victordavid626 <victordavid626@gmail.com>
2022-09-19 10:38:38 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,6 @@
 /**
  * puts_half - prints half of a string
  * @str: char to check
- *
  * Return: 0 is success
  */
 
@@ -15,16 +14,16 @@ void puts_half(char *str)
 
 		string++;
 
-		if (string + 1 % 2 != '0')
+	if (string + 1 % 2 != '0')
 
 		n = (string - 1) / 2;
 	else
 		n = (string / 2);
-
 		n++;
 
 	for (string = n; str[string] != '\0'; string++)
 
 		_putchar(str[string]);
+
 	_putchar('\n');
 }
