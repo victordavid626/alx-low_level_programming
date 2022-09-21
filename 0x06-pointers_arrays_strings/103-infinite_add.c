c410e4870bda068b90319a0aacd946f511de08b0
victordavid626 <victordavid626@gmail.com>
2022-09-21 15:34:59 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,51 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- *
- * Return: address of r or 0
+ * infinite_add - adds values of two strings together
+ * @n1: first string of integers
+ * @n2: second string of ints
+ * @r: result string
+ * @size_r: size of result string
+ * Return: void
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
-	for (i = 0; n1[i]; i++);
+	int len1 = 0, num1 = 0, sum1 = 0;
 
-	for (j = 0; n2[j]; j++);
+	int len2 = 0, num2 = 0, sum2 = 0;
 
-	if (i > size_r || j > size_r)
+	int num = 0, sum = 0, i, j;
 
+	char temp, n1c[1000], n2c[1000];
+
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+	if (len1 >= size_r || len2 >= size_r)
 	return (0);
-
-	m = 0;
-
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	for (j = 0, i = len1 - 1; j < len1; j++, i--)
+		n1c[j] = n1[i];
+		n1c[j] = '\0';
+	for (j = 0, i = len2 - 1; j < len2; j++, i--)
+		n2c[j] = n2[i];
+		n2c[j] = '\0';
+	while (num < size_r - 1)
 	{
-
-	n = m;
-
-	if (i >= 0)
-	n += n1[i] - '0';
-	if (j >= 0)
-	n += n2[j] - '0';
-	if (i < 0 && j < 0 && n == 0
-	{
+	if (num1 >= len1 && num2 >= len2 && sum == 0)
 	break;
+	sum1 = num1 < len1 ? n1c[num1] - '0' : 0;
+	sum2 = num2 < len2 ? n2c[num2] - '0' : 0;
+	sum += (sum1 + sum2);
+		r[num] = sum % 10 + '0';
+	sum = sum < 10 ? 0 : 1;
+	num++, num1++, num2++;
 	}
-	m = n / 10;
-	r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (sum != 0)
 	return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-	m = r[k];
-	r[k] = r[l];
-	r[l] = m;
-	}
+		r[num] = '\0';
+	for (j = 0, i = --num; j < num / 2; j++, i--)
+	temp = r[j], r[j] = r[i], r[i] = temp;
 	return (r);
 }
