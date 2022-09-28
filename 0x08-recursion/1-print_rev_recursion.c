a4dd86e71ee8227b726c9a64507d865ea83c19e4
victordavid626 <victordavid626@gmail.com>
2022-09-28 14:58:50 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - a function that returns the length of a string.
- *
- * @s: An input string to printing
- *
- * Return: The length of the string
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
-	return (1 + _strlen_recursion(s + 1));
-	return (0);
+	{
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
 }
