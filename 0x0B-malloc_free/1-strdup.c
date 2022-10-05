4ed723481f30c66091af1a204968f5e04b57108c
victordavid626 <victordavid626@gmail.com>
2022-10-04 21:05:28 -0700

    fixed bug

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,11 +20,14 @@ char *_strdup(char *str)
 
 	while (str[len] != '\0')
 		len++;
+
 	copy = (char *)malloc((sizeof(char) * len) + 1);
 	if (copy == NULL)
 		return (NULL);
+
 	for (i = 0; i < len; i++)
 		copy[i] = str[i];
-		copy[len] = '\0';
+	copy[len] = '\0';
+
 	return (copy);
 }
